62e9a384d6bb5d4739913b8b8450893b5a715b1c
Berin Aniesh <berinaniesh@gmail.com>
2022-08-31 09:03:24 +0530

    update

---
+++ lua/user/autocmd.lua
@@ -0,0 +1,4 @@
+--autocmd BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+--vim.api.nvim_create_autocmd(BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif)
+--autocmd BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+vim.cmd('source autocmd.vim')
---
+++ lua/user/autocmd.vim
@@ -0,0 +1,2 @@
+autocmd BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+
