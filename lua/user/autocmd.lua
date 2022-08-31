d133ea1d2113fb2fda1c6284d64d326ed6c34406
Berin Aniesh <berinaniesh@gmail.com>
2022-08-31 09:06:19 +0530

    update

--- lua/user/autocmd.lua
+++ lua/user/autocmd.lua
@@ -1,4 +1,4 @@
 --autocmd BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
 --vim.api.nvim_create_autocmd(BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif)
 --autocmd BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
-vim.cmd('source autocmd.vim')
+vim.cmd('source /home/berinaniesh/.config/nvim/lua/user/autocmd.vim')
